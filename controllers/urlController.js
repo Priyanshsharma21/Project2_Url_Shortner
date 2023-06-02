1358ec8d691629a0ee9fa0234d6f5a0804a4b366
vipulchandan <plenteousvipul90009@gmail.com>
2023-06-02 15:57:05 +0530

    Added functionality for caching with redis

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,6 +1,45 @@
 const URLModel = require('../models/urlModel')
 const validURL = require('valid-url')
 const shortId = require('shortid')
+const redis = require('redis')
+const { promisify } = require('util')
+
+const redisClient = redis.createClient(
+    12032,
+    "redis-12032.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+);
+redisClient.auth("spLbEbcKICkYWRvSORAanAYcfrLgCOhE", function (err) {
+    if (err) throw err;
+});
+  
+redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+});
+  
+//2. Prepare the functions for each command
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+// Connect to the Redis server
+// const redisClient = redis.createClient({
+//     port: 12032,
+//     host: "redis-12032.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+//     password: "spLbEbcKICkYWRvSORAanAYcfrLgCOhE",
+//     no_ready_check: true
+//   });
+  
+//   redisClient.on("connect", async function () {
+//     console.log("Connected to Redis..");
+//   });
+  
+//   redisClient.on("error", function (err) {
+//     console.error("Redis error:", err);
+//   });
+  
+//   // Prepare the functions for each Redis command
+//   const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+//   const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const createURL = async (req , res) => {
     try {
@@ -21,6 +60,16 @@ const createURL = async (req , res) => {
             })
         }
 
+        // Check if the short URL exists in the cache
+        const cachedShortUrl = await GET_ASYNC(longUrl);
+        if (cachedShortUrl) {
+            return res.status(200).send({
+                status: true,
+                message: "URL Found in Cache",
+                shortUrl: cachedShortUrl
+            });
+        }
+
         const urlCode = shortId.generate();
         const shortUrl = `${process.env.BASE_URL}/${urlCode}`;
 
@@ -29,6 +78,9 @@ const createURL = async (req , res) => {
             longUrl,
             shortUrl
         })
+
+        // Store the short URL in the cache
+        await SET_ASYNC(longUrl, shortUrl);
 
         res.status(201).send({
             status: true,
