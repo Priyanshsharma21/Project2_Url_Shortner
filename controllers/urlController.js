6f7e660d4d2ec4f1628f94d489d3ba6b5248aff1
Rahul <rahulmishra4317@gmial.com>
2023-06-02 16:29:32 +0530

    URL controller

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,67 +1,76 @@
-const URLModel = require('../models/urlModel')
-const validURL = require('valid-url')
+const URLModel = required('../models/urlModel.js')
+const validUrl = require('valid-url') 
 const shortId = require('shortid')
-const { long } = require('webidl-conversions')
-require('dotenv').config()
+const redis = require('redis')
 
-const { BASE_URL} = process.env
+const redisClient = redis.createClient(13834 , 'edis-13834.c264.ap-south-1-1.ec2.cloud.redislabs.com:13834', {no_ready_check : true})
 
-//1 connecting redis 
-
-const redisClient = redis.createClient(13834 ,'redis-13834.c264.ap-south-1-1.ec2.cloud.redislabs.com:13834' , {no_ready_check : true})
-
-redisClient.auth('qMoQtZEwlww9jgVLdlOWe7deRgw9vGqK',function(err){
-    if(err)  throw err
+redisClient.auth('qMoQtZEwlww9jgVLdlOWe7deRgw9vGq',function(err){
+    if(err) throw err
 })
 
-redisClient.on('connect', async function(){
-    console.log("Redis is connected")
+redisClient.on("connect", async function(){
+    console.log('connected to redis')
 })
 
-//2. Prepare the function for each command
+//2 Prepare the function 
 
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient)
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient)
 
-const createURL = async (req , res) => {
-    try {const {longUrl} = req.body
+const createURL = async (req,res) => {
+    try {
 
-    if(!longUrl) {
-        return res.status(400).send({status : false , message : "Missing longUrl"})
+        const {longUrl } = req.body 
+        if(!longUrl) {
+            return res.status(400).send({status : false , message : "Missing long URL" })
+        }
+
+        if(!validUrl.isWebUri(longUrl)) {
+            return res.status(400).send({status : false , message : "Invalid Long URL"})
+
+        }
+
+        //checking wheather the short url exists in the cache or not 
+        const cachedShortUrl = await GET_ASYNC(longUrl)
+        if(cachedShortUrl) {
+            return res.status(200).send({status : true , message : "URL found in cache",shortURL : cachedShortUrl})
+        }
+
+        const urlCode = shortId.generate()
+        const shortId = `${process.env.BASE_URL}/${urlCode}`
+
+        const data = await URLModel.create({urlCode , longUrl , shortUrl})
+
+        await SET_ASYNC(longUrl , shortId)
+
+        res.status(201).send({status : true , message : "URL Created Successfully",data : data})
+   } catch(error) {
+    res.status(500).send({Status : false , error : error.message })
+   }
+
+   const getUrl = async (req,res) => {
+    try { 
+        const {urlCode} = req.params
+        const url = await URLModel.findOne({urlCode})
+
+        if(!url) {
+            return res.status(400).send({status : false , message : 'URL not found'})
+        }
+
+        return res.redirect(302 , url.longUrl)
+    }catch(error) {
+        res.status(500).send({status : false , message : error.message})
     }
+   }
 
-    if(!validURL.isUri(longUrl)) {
-        return res.status(400).send({status : false , message : "Invalid longUrl"})
-    }
+   module.exports = {
+    createURL , 
+    getUrl
+   }
 
-    const urlCode = shortId.generate()
 
-    const shortUrl = `http://localhost:3000/${urlCode}`
 
-    const data = await URLModel.create({
-        urlCode ,
-        longUrl ,
-        shortUrl
-    })
-    res.status(201).send({
-        status : true , 
-        data : data 
-    })
-} catch (error) {
-    return res.status(500).send({status : flase , message : error.message})
-    }
+
+
 }
-
-const fetchURLProfile = async function(req,res) {
-    let cachedProfileData = await GET_ASYNC(`${req.params.authorId}`)
-    if(cachedProfileData) {
-        res.send(cachedProfileData)
-    }else {
-        let profile = await URLModel.findById(req.params.authorId)
-        await SET_ASYNC(`${req.params.authorId}`,JSON.stringify(profile))
-        res.send({data : profile})
-    }
-}
-
-module.exports.createURL = createURL
-module.exports.fetchURLProfile = fetchURLProfile
